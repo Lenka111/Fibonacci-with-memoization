65e07478d8e90320370e32c0e454ce95d3907480
Elena Voinu <37947819+Lenka111@users.noreply.github.com>
2020-03-20 14:19:23 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,7 @@ import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
+        //new instance
         MemoizationFibonacci mf = new MemoizationFibonacci(10);
         int []array = new int[100];
         mf.fibNum(7);
